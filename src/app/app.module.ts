1af8c9358275bc55e38759b719621c8aa68c3422
AymaneMALIH <aymane.malih@edu.uca.ma>
2022-03-09 10:22:17 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AdminModule} from "./view/admin/admin.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AdminModule,
     BrowserModule,
     AppRoutingModule
   ],
